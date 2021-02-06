1df2f6e28a005acc7e7ee85dbb9449a1bc98b333
Karolina Wieczorek <karolinachrop@gmail.com>
2021-02-06 21:44:19 +0100

    Add BookListAdapter and dataclass Book. Change book_list_one_row.

---
+++ app/src/main/java/com/example/ksiegarniarobooszka/View/Book.kt
@@ -0,0 +1,3 @@
+package com.example.ksiegarniarobooszka.View
+
+data class Book(val author:String, val genre:String, val id:Int, val price: Double, val quantity:Int, val image: String, val title:String)
--- app/src/main/java/com/example/ksiegarniarobooszka/View/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/ksiegarniarobooszka/View/Fragments/SearchFragment.kt
@@ -10,15 +10,38 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ksiegarniarobooszka.R
+import com.example.ksiegarniarobooszka.View.Book
+import com.example.ksiegarniarobooszka.View.BookListAdapter
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment(){
+
+    private lateinit var bookAdapter: BookListAdapter
+    private lateinit var bookLayoutManager: LinearLayoutManager
+    private lateinit var bookRecyclerView: RecyclerView
+    private lateinit var products:DatabaseReference
+    var listOfItems = ArrayList<Book>()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val firebase = FirebaseDatabase.getInstance()
+        products = firebase.getReference("products")
+        bookLayoutManager = LinearLayoutManager(context)
+        listOfItems.add(Book("Ktoś z długim nazwiskiem, żeby zobaczyć, co się dzieje","Kryminał",1,21.50,15,"https://firebasestorage.googleapis.com/v0/b/ksiegarniarobooszka.appspot.com/o/AW-Trasa%20promocyjna.jpg?alt=media&token=284c8b77-7730-4855-a1ca-062ae72c86ac","Tytuł"))
+        listOfItems.add(Book("Jakub Ćwiek","Kryminał",2,45.50,10,"https://firebasestorage.googleapis.com/v0/b/ksiegarniarobooszka.appspot.com/o/J%C4%86-K%C5%82amca.jpg?alt=media&token=ac98cb43-65a6-408b-92d2-67c490c4969c","Kłamca"))
+        bookAdapter= BookListAdapter(listOfItems, context!!)
+
+
         return inflater.inflate(R.layout.fragment_search,container,false)
     }
 
@@ -30,7 +53,11 @@ class SearchFragment : Fragment(){
                 "Wszystkie",
                 "Kryminały",
                 "Poradniki",
-                "Horrory"
+                "Horrory",
+                "Komiksy",
+                "Fantastyka",
+                "Thrillery",
+                "Science-Fiction"
             )
             val adapter: ArrayAdapter<String> = object : ArrayAdapter<String>(
                 context, android.R.layout.simple_spinner_dropdown_item,
@@ -55,5 +82,19 @@ class SearchFragment : Fragment(){
             }
             spinnerBookCategories.adapter = adapter
         }
+
+        bookRecyclerView=recycler_search.apply {
+            this.layoutManager = bookLayoutManager
+            this.adapter = bookAdapter
+        }
+
+
+//        val firebase = FirebaseDatabase.getInstance()
+//        products = firebase.getReference("products")
+//        bookRecyclerView = view.findViewById(R.id.recycler_search)
+
+
+
     }
+
 }
