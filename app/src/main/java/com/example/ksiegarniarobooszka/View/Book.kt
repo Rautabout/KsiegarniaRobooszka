cac196d85123bf7fa10b93ef46e222d0eda2efa9
Krzysztof Piątkowski <ra.krzysztof.piatkowski@gmail.com>
2021-02-07 00:29:42 +0100

    Revert "mergin into rta"
    This reverts commit 602840d9a032b838c3bc7901806618686066ed51, reversing
    changes made to 17dda4681290b9dfb816d51be483a4a86d727139.

--- app/src/main/java/com/example/ksiegarniarobooszka/View/Book.kt
+++
@@ -1,3 +0,0 @@
-package com.example.ksiegarniarobooszka.View
-
-data class Book(val author:String?=null, val genre:String?=null, val id:Int?=null, val price: Double?=null, val quantity:Int?=null, val thumbnail_url: String?=null, val title:String?=null)
